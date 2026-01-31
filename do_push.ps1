29025fcb9b6b096417fa22fc789f2651af5c9721
ussoewwin <136552381+ussoewwin@users.noreply.github.com>
2026-01-31 20:22:58 +0900

    Remove do_push.ps1 and do_tag.ps1 from repo (keep in .gitignore)

--- do_push.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location "d:\USERFILES\GitHub\hswq"
-Remove-Item -Force ".git\index.lock" -ErrorAction SilentlyContinue
-Get-ChildItem ".git\objects\c6\tmp_obj_*" -ErrorAction SilentlyContinue | Remove-Item -Force
-git add -A
-git commit -m "Add ComfyUI-master, bulk file/folder changes, English comments"
-git push
